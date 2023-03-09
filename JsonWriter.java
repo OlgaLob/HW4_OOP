4b527855609a7c083d8638f18e19ade8222faf19
Olga Lobanova <arapova@list.ru>
2023-03-09 12:00:51 +0300

    Format JSON-file

---
+++ JsonWriter.java
@@ -0,0 +1,11 @@
+package HWs.HW4.MyPlanner;
+
+public class JsonWriter implements Format {
+    @Override
+    public <T extends Task> String createString(T task) {
+        return String.format(
+                "id: %d ,Task: %s, author: %s adding date: %s, deadline: %s, priority: %s"
+                , task.getId(), task.getSubject(), task.getAuthor(), task.getAddTime(),
+                task.getAddDate(), task.getEndOfTask(), task.getPriority());
+    }
+}
