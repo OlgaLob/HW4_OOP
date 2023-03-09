efed8e6b3f015efd35dee04c9143d80ce8e1a767
Olga Lobanova <arapova@list.ru>
2023-03-09 12:09:22 +0300

    Next and HasNext methods

---
+++ PlannerIterator.java
@@ -0,0 +1,22 @@
+package HWs.HW4.MyPlanner;
+
+import java.util.Iterator;
+
+public class PlannerIterator implements Iterator<Task> {
+    private int index = -1;
+    private final Planner tasks;
+
+    public PlannerIterator(Planner tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < tasks.getSize() - 1;
+    }
+
+    @Override
+    public Task next() {
+        return tasks.getTask(++index);
+    }
+}
